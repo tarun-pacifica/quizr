626c5b9df42c8a0387ffe87299abf48320d59c61
Dan Addington <daniel.anthony.addington@gmail.com>
2013-08-22 17:45:30 +1000

    progress

--- app/controllers/user_answers_controller.rb
+++ app/controllers/user_answers_controller.rb
@@ -2,7 +2,7 @@ class UserAnswersController < ApplicationController
   def create
     answer = Answer.where(:id => params[:user_answer][:answer_id]).first
     @quiz = answer.question.quiz
-    @user_quiz = UserQuiz.where(:quiz_id => @quiz.id).first
+    @user_quiz = @current_user.user_quizzes.where(:quiz_id => @quiz.id).first
     @user_answer = UserAnswer.new(:answer_id => answer.id, :question_id => answer.question_id, :user_quiz_id => @user_quiz.id )
       if @user_answer.save
         redirect_to @quiz
